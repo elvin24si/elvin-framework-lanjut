ecf635a06c2a2bdb6247f176012c55b17aba44f1
elvin24si <elvin24si@mahasiswa.pcr.ac.id>
2026-03-13 15:16:03 +0700

    First Commit

---
+++ src/pertemuan-2/HelloWorld.jsx
@@ -0,0 +1,56 @@
+export default function HelloWorld(){
+    const propsUserCard = {
+        nama: "Goku",
+        nim: "999999",
+        tanggal: "2025-01-01"
+    }
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>Selamat Belajar ReactJs</p>
+
+            <GreetingBinjai/>
+
+            <QuoteText/>
+
+            <UserCard 
+	            nama="Fikri" 
+	            nim="169412"
+	            tanggal={new Date().toLocaleDateString()}
+	          />
+            <UserCard{...propsUserCard}/>
+
+            <img src="img/duck.gif" alt="duck"/>
+
+        </div>
+    )
+}
+
+function GreetingBinjai(){
+    return (
+        <small>Salam dari Binjai</small>
+    );
+}
+
+function QuoteText() {
+    const text = "Mulutmu Bebekmu";
+    const text2 = "Aku ingin jadi bebek";
+    return (
+        <div>
+            <hr/>
+            <p>{text.toLowerCase()}</p>
+            <p>{text2.toUpperCase()}</p>
+        </div>
+    )
+}
+
+function UserCard(props){
+    return (
+        <div>
+            <hr/>
+            <h3>Nama: {props.nama}</h3>
+            <p>NIM: {props.nim}</p>
+            <p>Tanggal: {props.tanggal}</p>
+        </div>
+    )
+}
